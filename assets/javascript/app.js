f5c1bba7c3f621ea2f631c891849adb1d3aeab27
Hunter Wiegand <hunter_wiegand@yahoo.com>
2019-03-08 20:38:58 -0800

    Removed result timer and replaced with

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,20 +11,59 @@ var game = {
     correct: "0",
     incorrect: "0",
 
-    questionCounter: 1,
+    questionCounter: 0,
 
+    intervalId: "",
+    gameTimer: 7,
+    resultTimer: 5,
+
+    //Function to start game off buton
     startGame: function () {
         $("#start-button").on("click", function () {
+            game.runGameTimer();
             $("#title-message").html("TRIVIA TIME");
-            $("#start-button").html("<div id='timer-card'><span>Timer card goes here</span></div>");
+            $("#start-button").html("<div id='timer-card'><span></span></div>");
             $("#question-container").removeClass("hidden");
+
+            game.updateDisplay();
         })
     },
 
+    //function to run game timer
+    runGameTimer: function () {
+        clearInterval(game.intervalId);
+        game.intervalId = setInterval(game.decrementGameTimer, 1000);
+    },
+
+    //Function to count down game timer
+    decrementGameTimer: function () {
+
+        game.gameTimer--;
+        $("#timer-card").html("<h3>Remaining time: " + game.gameTimer + "</h3>");
+
+        if (game.gameTimer === 0) {
+            game.stopGameTimer();
+            console.log("STAPH");
+            game.displayOutOfTime();
+        }
+    },
+
+    //Function to stop game timer
+    stopGameTimer: function () {
+        clearInterval(game.intervalId);
+        game.generateQuestion();
+        // game.updateDisplay();
+        setTimeout(game.updateDisplay, 3000);
+        if (game.questionCounter > 10) {
+            game.displayResults();
+        }
+    },
+
+    //function to generate trivia questions
     generateQuestion: function () {
 
         switch (game.questionCounter) {
-            case 1:
+            case 0:
                 game.question = "What is my name";
                 game.option1 = "Tom";
                 game.option2 = "Tim";
@@ -33,37 +72,41 @@ var game = {
                 game.answer = "3";
                 game.answerAsStr = "Hunter";
                 break;
-            case 2:
+            case 1:
                 game.question = "What is my dog name";
                 game.option1 = "Tom";
                 game.option2 = "Tim";
                 game.option3 = "Hunter";
                 game.option4 = "Ty";
-                game.answer = "Hunter";
+                game.answer = "3";
+                game.answerAsStr = "Hunter";
                 break;
-            case 3:
+            case 2:
                 game.question = "What is my hat name";
                 game.option1 = "Tom";
                 game.option2 = "Tim";
                 game.option3 = "Hunter";
                 game.option4 = "Ty";
-                game.answer = "Hunter";
+                game.answer = "3";
+                game.answerAsStr = "Hunter";
                 break;
-            case 4:
+            case 3:
                 game.question = "What is my bob name";
                 game.option1 = "Tom";
                 game.option2 = "Tim";
                 game.option3 = "Hunter";
                 game.option4 = "Ty";
-                game.answer = "Hunter";
+                game.answer = "3";
+                game.answerAsStr = "Hunter";
                 break;
-            case 5:
+            case 4:
                 game.question = "What is my feend name";
                 game.option1 = "Tom";
                 game.option2 = "Tim";
                 game.option3 = "Hunter";
                 game.option4 = "Ty";
-                game.answer = "Hunter";
+                game.answer = "3";
+                game.answerAsStr = "Hunter";
                 break;
             case 6:
                 game.question = "What is my silly name";
@@ -71,7 +114,8 @@ var game = {
                 game.option2 = "Tim";
                 game.option3 = "Hunter";
                 game.option4 = "Ty";
-                game.answer = "Hunter";
+                game.answer = "3";
+                game.answerAsStr = "Hunter";
                 break;
             default:
                 game.question = "";
@@ -80,41 +124,56 @@ var game = {
                 game.option3 = "";
                 game.option4 = "";
                 game.answer = "";
+                game.answerAsStr= "";
                 break;
         }
-
-        game.updateDisplay();
     },
 
+    //function to get player input
     getPlayerGuess: function () {
-        $(".select-answer").on("click", function () {
+
+        $(document).on("click", ".select-answer", function () {
             var playerGuess = this.value;
 
             game.applyGuess(playerGuess);
-            game.questionCounter++;
+            // game.questionCounter++;
+
+
             game.generateQuestion();
-            game.updateDisplay();
         });
     },
 
+    //function to evaluate player input
     applyGuess: function (guess) {
 
         console.log("Guess ", guess);
         console.log("Answer ", game.answer);
 
         if (guess === game.answer) {
+            game.stopGameTimer();
+
             console.log("You guessed right");
             game.displayCorrectGuess();
             game.correct++;
         } else {
+            game.stopGameTimer();
             console.log("Wrong!!!!!!!");
             game.displayIncorrectGuess();
             game.incorrect++;
         }
+
+        // game.updateDisplay();
     },
 
+
+    //Update Screen for questions
     updateDisplay: function () {
 
+        game.runGameTimer();
+
+        game.questionCounter++;
+
+        $("#timer-card").removeClass("hidden");
 
         $("#option1").removeClass("hidden");
         $("#option2").removeClass("hidden");
@@ -131,30 +190,78 @@ var game = {
         $("#option4").html(game.option4);
     },
 
+    //Display Correct Result Screen
     displayCorrectGuess: function () {
 
-        $("#question-counter").html("You guessed correct!");
+        $("#question-text").html("You guessed correct!");
 
+        //set timeout for 5 seconds to change displayCorrect guess to display
+
+        // game.runResultTimer();
+
+        console.log("You guessed rightttt");
+
+        //Hide timer card
+        $("#timer-card").addClass("hidden");
+
+        // $("#question-counter").html("Question " + game.questionCounter - 1);
         $("#answer-row-display").removeClass("hidden");
-
         $("#answer-display").html(game.answerAsStr);
         $("#option1").addClass("hidden");
         $("#option2").addClass("hidden");
         $("#option3").addClass("hidden");
         $("#option4").addClass("hidden");
+
+        game.gameTimer = 7;
     },
 
+    //Display Incorrect Result Screen
     displayIncorrectGuess: function () {
 
         $("#question-text").html("Incorrect, the answer was");
 
+        //set timeout for 5 seconds to change displayCorrect guess to display
+        
+        // game.runResultTimer();
+        //Hide timer card
+        $("#timer-card").addClass("hidden");
 
+        // $("#question-counter").html("Question " + game.questionCounter - 1);
         $("#answer-row-display").removeClass("hidden");
         $("#answer-display").html(game.answerAsStr);
         $("#option1").addClass("hidden");
         $("#option2").addClass("hidden");
         $("#option3").addClass("hidden");
         $("#option4").addClass("hidden");
+
+        game.gameTimer = 7;
+    },
+
+    //Display Out of time Sceen
+    displayOutOfTime: function () {
+
+        $("#question-text").html("You ran out of time!, the answer was");
+
+        //set timeout for 5 seconds to change displayCorrect guess to display
+
+        //Hide timer card
+        $("#timer-card").addClass("hidden");
+        
+        // game.runResultTimer();
+
+        // $("#question-counter").html("Question " + game.questionCounter - 1);
+        $("#answer-row-display").removeClass("hidden");
+        $("#answer-display").html(game.answerAsStr);
+        $("#option1").addClass("hidden");
+        $("#option2").addClass("hidden");
+        $("#option3").addClass("hidden");
+        $("#option4").addClass("hidden");
+
+        game.gameTimer = 7;
+    },
+
+    displayResults: function () {
+        
     }
 };
 
@@ -162,4 +269,3 @@ game.generateQuestion();
 game.startGame();
 
 game.getPlayerGuess();
-
